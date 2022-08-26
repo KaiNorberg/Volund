23932a3d8afd6cd4dfd448f84265bc1cded11ee2
Kaj9296 <73393684+Kaj9296@users.noreply.github.com>
2022-08-26 10:37:06 +0200

    Changed Math from a namespace to a class.

--- Volund/src/Math/Math.cpp
+++ Volund/src/Math/Math.cpp
@@ -2,9 +2,25 @@
 
 #include "Math.h"
 
-namespace Volund::Math
+namespace Volund
 {
-	Vec3 Euler(Quat const& Q)
+	 const Vec3 Math::Up = Vec3(0, 1, 0);
+
+	 const Vec3 Math::Down = Vec3(0, -1, 0);
+
+	 const Vec3 Math::Forward = Vec3(0, 0, 1);
+	  
+	 const Vec3 Math::Back = Vec3(0, 0, -1);
+
+	 const Vec3 Math::Left = Vec3(-1, 0, 0);
+
+	 const Vec3 Math::Right = Vec3(1, 0, 0);
+
+	 const Vec3 Math::One = Vec3(1, 1, 1);
+
+	 const Vec3 Math::Zero = Vec3(0, 0, 0);
+
+	Vec3 Math::Euler(Quat const& Q)
 	{
 		return ToDegrees(glm::eulerAngles(Q));
 	}
--- Volund/src/Math/Math.h
+++ Volund/src/Math/Math.h
@@ -7,71 +7,76 @@
 
 #define PI 3.14159265359f
 
-namespace Volund::Math
+namespace Volund
 {		
-	/// <summary>
-	/// A vector pointing up.
-	/// </summary>
-	static const Vec3 Up = Vec3(0, 1, 0);
-
-	/// <summary>
-	/// A vector pointing down.
-	/// </summary>
-	static const Vec3 Down = Vec3(0, -1, 0);
-
-	/// <summary>
-	/// A vector pointing Forward.
-	/// </summary>
-	static const Vec3 Forward = Vec3(0, 0, 1);
-
-	/// <summary>
-	/// A vector pointing Backward.
-	/// </summary>
-	static const Vec3 Back = Vec3(0, 0, -1);
-
-	/// <summary>
-	/// A vector pointing Left.
-	/// </summary>
-	static const Vec3 Left = Vec3(-1, 0, 0);
-
-	/// <summary>
-	/// A vector pointing Right.
-	/// </summary>
-	static const Vec3 Right = Vec3(1, 0, 0);
-
-	/// <summary>
-	/// A vector filled with ones.
-	/// </summary>
-	static const Vec3 One = Vec3(1, 1, 1);
-
-	/// <summary>
-	/// A vector filled with zeros.
-	/// </summary>
-	static const Vec3 Zero = Vec3(0, 0, 0);
-
-	Vec3 Euler(Quat const& Q);
-
-	template <class T>
-	T ToRadians(T const& V)
+	class Math
 	{
-		return (PI / 180.0f) * V;
-	}
+	public:
+		/// <summary>
+		/// A vector pointing up.
+		/// </summary>
+		static const Vec3 Up;
 
-	template <class T>
-	T ToDegrees(T const& V)
-	{
-		return (180.0f / PI) * V;
-	}
+		/// <summary>
+		/// A vector pointing down.
+		/// </summary>
+		static const Vec3 Down;
 
-	template <class T>
-	T Min(T N1, T N2)
-	{
-		return (N1 < N2) ? N1 : N2;
-	}
+		/// <summary>
+		/// A vector pointing Forward.
+		/// </summary>
+		static const Vec3 Forward;
 
-	template <class T>
-	T Max(T N1, T N2)
-	{
-		return (N1 > N2) ? N1 : N2;
-	}
+		/// <summary>
+		/// A vector pointing Backward.
+		/// </summary>
+		static const Vec3 Back;
+
+		/// <summary>
+		/// A vector pointing Left.
+		/// </summary>
+		static const Vec3 Left;
+
+		/// <summary>
+		/// A vector pointing Right.
+		/// </summary>
+		static const Vec3 Right;
+
+		/// <summary>
+		/// A vector filled with ones.
+		/// </summary>
+		static const Vec3 One;
+
+		/// <summary>
+		/// A vector filled with zeros.
+		/// </summary>
+		static const Vec3 Zero;
+
+		static Vec3 Euler(Quat const& Q);
+
+		template <class T>
+		static T ToRadians(T const& V)
+		{
+			return (PI / 180.0f) * V;
+		}
+
+		template <class T>
+		static T ToDegrees(T const& V)
+		{
+			return (180.0f / PI) * V;
+		}
+
+		template <class T>
+		static T Min(T N1, T N2)
+		{
+			return (N1 < N2) ? N1 : N2;
+		}
+
+		template <class T>
+		static T Max(T N1, T N2)
+		{
+			return (N1 > N2) ? N1 : N2;
+		}
+	private:
+	};
 }
