1b38c88f4f13358e4a6ec402ce543a6fe43e86f7
Kaj9296 <73393684+Kaj9296@users.noreply.github.com>
2023-02-15 22:46:58 +0100

    Update Application.cpp

--- Volund/src/Application/Application.cpp
+++ Volund/src/Application/Application.cpp
@@ -39,28 +39,24 @@ namespace Volund
 	{
 		VOLUND_PROFILE_FUNCTION();
 
-		std::chrono::time_point<std::chrono::steady_clock> OldTime = std::chrono::high_resolution_clock::now();
+		std::chrono::time_point<std::chrono::steady_clock> StartTime = std::chrono::high_resolution_clock::now();
 
 		while (this->_ShouldRun)
 		{
 			VOLUND_PROFILE_SCOPE("MainLoop");
 
-			std::chrono::duration<double> Duration = std::chrono::high_resolution_clock::now() - OldTime;
-			OldTime = std::chrono::high_resolution_clock::now();
-			TimeStep TS = TimeStep(Duration.count());
+			TimeStep TS = TimeStep(std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - StartTime).count());
+			StartTime = std::chrono::high_resolution_clock::now();
 
 			this->_ThreadPool.Submit([this, TS]()
 			{
-				Event E = Event(EventType::UPDATE);
-				VOLUND_EVENT_UPDATE_SET_TIMESTEP(E, float(TS));
-				this->_EventDispatcher->Dispatch(E);
+				Event UpdateEvent = Event(EventType::UPDATE);
+				VOLUND_EVENT_UPDATE_SET_TIMESTEP(UpdateEvent, float(TS));
+				this->_EventDispatcher->Dispatch(UpdateEvent);
 			});
 
 			Renderer::Begin();
-
-			Event E = Event(EventType::RENDER);
-			this->_EventDispatcher->Dispatch(E);
-
+			this->_EventDispatcher->Dispatch(Event(EventType::RENDER));
 			Renderer::End();
 
 			while (this->_ThreadPool.Busy());
